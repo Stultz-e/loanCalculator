2288d6c33e230bd06125e2927cddedcc124f8de6
cstultz87 <cstultz@oswego.edu>
2019-02-05 10:33:57 -0600

    dude nyce

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 // Listen for submit 
-document.getElementById('loan-form').addEventListener('submit', calculateResult);
+document.getElementById('loan-form').addEventListener('submit', function(e) {
+    //Hide results
+
+    // Show loader
+    document.getElementById('loading').style.display = 'block';
+
+    setTimeout(calculateResults, 2000);
+
+    e.preventDefault();
+});
 
 // Calculate Results
-function calculateResult(e) {
+function calculateResults(e) {
     console.log('Calculating...');
     // UI Vars
     const amount = document.getElementById('amount');
@@ -24,11 +33,16 @@ function calculateResult(e) {
         monthlyPayment.value = monthly.toFixed(2);
         totalPayment.value = (monthly * calculatedPayments).toFixed(2);
         totalInterest.value = ((monthly * calculatedPayments)-principal).toFixed(2);
+
+        // Show results
+        document.getElementById('results').style.display = 'block';
+
+        // Hide loader 
+        document.getElementById('loading').style.display = 'none';
+
     } else {
         showError('Please check your numbers');
     }
-
-    e.preventDefault();
 }
 
 // Show Error 
